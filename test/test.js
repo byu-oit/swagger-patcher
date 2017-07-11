d172742fcf62d3b55981a1a2064c66f5df7bd728
Aaron Rasmussen <aaron.rasmussen@byu.edu>
2017-07-11 12:19:46 -0600

    patch functionality working

--- test/test.js
+++ test/test.js
@@ -5,9 +5,37 @@ const testData = require('./testData')
 
 test('simple key', assert => {
   {
-    const actual = patch(testData.swagger, testData.testPatch1)
-    const expected = testData.testResult1
+    const actual = patch(testData.swagger, testData.test1Patch1)
+    const expected = testData.test1Result1
     assert.deepEqual(actual, expected, 'Patching 1 simple key worked')
+  }
+
+  {
+    const actual = patch(testData.swagger, testData.test1Patch2)
+    const expected = testData.test1Result2
+    assert.deepEqual(actual, expected, 'Patching multiple simple keys worked')
+  }
+
+  assert.end()
+})
+
+test('compound key', assert => {
+  {
+    const actual = patch(testData.swagger, testData.test2Patch1)
+    const expected = testData.test2Result1
+    assert.deepEqual(actual, expected, 'Patching 1 compound key worked')
+  }
+
+  {
+    const actual = patch(testData.swagger, testData.test2Patch2)
+    const expected = testData.test2Result2
+    assert.deepEqual(actual, expected, 'Patching array compound key worked')
+  }
+
+  {
+    const actual = patch(testData.swagger, testData.test2Patch3)
+    const expected = testData.test2Result3
+    assert.deepEqual(actual, expected, 'Patching multiple compound key worked')
   }
 
   assert.end()
--- test/testData.js
+++ test/testData.js
@@ -12,29 +12,120 @@ const swagger = {
     'description': 'Field Two\'s original dexription',
     'type': 'Object',
     'example': {
-      'value': 'Two'
+      'value': 'Two',
+      'description': 'this is example data'
     }
   },
   'field_three': {
-    'description': 'A description for field three',
-    'type': 'Object',
-    'example': {
-      'value': 'Three'
-    }
+    'properties': [
+      {
+        'description': 'nested object 1',
+        'type': 'Object',
+        'example': {
+          'value': '3.1'
+        }
+      },
+      {
+        'description': 'nested object 2',
+        'type': 'Object',
+        'example': {
+          'value': '3.2'
+        }
+      }
+    ]
   }
 }
 
-const testPatch1 = [
+const test1Patch1 = [
   {
     key: 'field_one',
     description: 'The new description'
   }
 ]
 
-const testResult1 = _.cloneDeep(swagger)
+const test1Result1 = ((orig) => {
+  const copy = _.cloneDeep(orig)
+  copy['field_one']['description'] = 'The new description'
+  return copy
+})(swagger)
+
+const test1Patch2 = [
+  {
+    key: 'field_one',
+    description: 'The new description'
+  },
+  {
+    key: 'field_two',
+    description: 'Field Two\'s updated description'
+  }
+]
+
+const test1Result2 = ((orig) => {
+  const copy = _.cloneDeep(orig)
+  copy['field_one']['description'] = 'The new description'
+  copy['field_two']['description'] = 'Field Two\'s updated description'
+  return copy
+})(swagger)
+
+const test2Patch1 = [
+  {
+    key: 'field_two.example',
+    description: 'new example data'
+  }
+]
+
+const test2Result1 = ((orig) => {
+  const copy = _.cloneDeep(orig)
+  copy['field_two']['example']['description'] = 'new example data'
+  return copy
+})(swagger)
+
+const test2Patch2 = [
+  {
+    key: 'field_three.properties.[0]',
+    description: 'updated nested object 1'
+  }
+]
+
+const test2Result2 = ((orig) => {
+  const copy = _.cloneDeep(orig)
+  copy['field_three']['properties'][0]['description'] = 'updated nested object 1'
+  return copy
+})(swagger)
+
+const test2Patch3 = [
+  {
+    key: 'field_two.example',
+    description: 'new example data'
+  },
+  {
+    key: 'field_three.properties.[0]',
+    description: 'updated nested object 1'
+  },
+  {
+    key: 'field_three.properties.[1]',
+    description: 'updated nested object 2'
+  }
+]
+
+const test2Result3 = ((orig) => {
+  const copy = _.cloneDeep(orig)
+  copy['field_two']['example']['description'] = 'new example data'
+  copy['field_three']['properties'][0]['description'] = 'updated nested object 1'
+  copy['field_three']['properties'][1]['description'] = 'updated nested object 2'
+  return copy
+})(swagger)
 
 module.exports = {
   swagger,
-  testPatch1,
-  testResult1
+  test1Patch1,
+  test1Result1,
+  test1Patch2,
+  test1Result2,
+  test2Patch1,
+  test2Result1,
+  test2Patch2,
+  test2Result2,
+  test2Patch3,
+  test2Result3
 }
